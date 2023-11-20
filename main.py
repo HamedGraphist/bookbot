ee73a52d176411e6334c67eaf05b38f21cf5fc55
HamedGraphist <hamed.momeni.ir@gmail.com>
2023-11-20 13:59:28 +0330

    Your commit message

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello world")
+path_to_file = "C:\\Users\\user\\workspace\\github.com\\HamedGraphist\\bookbot\\books"
+
+# Use a with block to open the file
+with open(path_to_file, 'r') as file:
+    # Read the contents of the file into a string
+    file_contents = file.read()
+
+# Now you can work with the file_contents variable, which contains the content of the file
+print(file_contents)
