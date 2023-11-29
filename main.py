adeda64c45f8804d2d596d6901006f9b045e7fd0
HamedGraphist <hamed.momeni.ir@gmail.com>
2023-11-29 14:36:09 +0330

    This project is finished

--- main.py
+++ main.py
@@ -28,13 +28,13 @@ path_to_file = r'C:\Users\user\workspace\github.com\HamedGraphist\bookbot\books\
 
 
 
-# Use a with block to open the file
+
 with open(path_to_file) as file:
-    # Read the contents of the file into a string
+    
     file_contents = file.read()
     
 
-# Now you can work with the file_contents variable, which contains the content of the file
+
 print(file_contents)
 generate_report(file_contents)
 
