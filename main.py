5ad6087270efd5743595c45a164eba46fb634a27
HamedGraphist <hamed.momeni.ir@gmail.com>
2023-11-29 14:15:54 +0330

    the project is finished

--- main.py
+++ main.py
@@ -1,12 +1,40 @@
-# main.py
+def count_words(text):
+    words = text.split()
+    return len(words)
 
-# Specify the path to the file (replace with the actual path)
-path_to_file = r"C:\Users\user\workspace\github.com\HamedGraphist\bookbot\books\frankenstein.txt"
+def count_charactors(text):
+    text = text.lower()
+    char_count = {}
+    for char in text:
+        char_count[char] = char_count.get(char, 0) + 1
+    return char_count
+
+def get_count(item):
+    return item[1]
+
+def generate_report(file_contents):
+    num_words = count_words(file_contents)
+    num_char = count_charactors(file_contents)
+
+    sorted_charactors = sorted(num_char.items(),key=get_count, reverse=True)
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{num_words} is the Number of the words")
+
+    for char, count in sorted_charactors:
+        if char.isalpha():
+            print(f"The {char} character was found {count} times")
+    print("--- End Report ---")
+path_to_file = r'C:\Users\user\workspace\github.com\HamedGraphist\bookbot\books\frankenstein.txt'
+
+
 
 # Use a with block to open the file
-with open(path_to_file, 'r') as file:
+with open(path_to_file) as file:
     # Read the contents of the file into a string
     file_contents = file.read()
+    
 
 # Now you can work with the file_contents variable, which contains the content of the file
 print(file_contents)
+generate_report(file_contents)
+
